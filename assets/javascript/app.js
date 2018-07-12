a1d066b80848669c831329b99f2a537039200ed2
Joshua Beckford <joshua93beckford@gmail.com>
2018-07-11 20:35:44 -0400

    added distance to table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,7 +21,7 @@ var hadd;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 6
+        zoom: 12
     });
 
 
@@ -50,17 +50,7 @@ function initMap() {
                     console.log(reply.statuses.length);
 
                     for(var i = 0; i < reply.statuses.length; i++){
-                        var row = $("<tr>");
-                        row.append("<td>" + reply.statuses[i].user.screen_name)
-                        row.append("<td>" + reply.statuses[i].user.name)
-                        row.append("<td>")
-                        row.append("<td>" + reply.statuses[i].text);
-    
-                        if(reply.statuses[i].entities.urls.length !== 0){
-
-                            row.append("<td> <a href=" + reply.statuses[i].entities.urls[0].url + ">" + reply.statuses[i].entities.urls[0].url);
-                        }
-                        $("tbody").append(row);
+                        
                     }
 
 
@@ -76,6 +66,19 @@ function initMap() {
                             };
                             console.log(lpos)
                             addMarker(lpos);
+                            var row = $("<tr>");
+                        row.append("<td>" + loc.user.screen_name)
+                        row.append("<td>" + loc.user.name)
+                        var p1 = parseFloat(loc.coordinates.coordinates[1]);
+                        var p2 = parseFloat(loc.coordinates.coordinates[0]);
+                        row.append("<td>" + parseInt(getDistance(pos,lpos)))
+                        row.append("<td>" + loc.text);
+    
+                        if(loc.entities.urls.length !== 0){
+
+                            row.append("<td> <a href=" + loc.entities.urls[0].url + ">" + loc.entities.urls[0].url);
+                        }
+                        $("tbody").append(row);
                         }
                         
 
@@ -137,10 +140,7 @@ function getDistance(p1, p2) {
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c;
 
-    firebase.database().ref("/incident").update({
-        "distance": d
-    });
-    $('#content').append('<h1 style="color:white" >' + d + '</h1>'); // returns the distance in meter
+    return d;
 }
 
 function rad(x) {
